3cd459f1fbf910fe280aa9c002b004d03b94774e
MLucasM <martylucas95@gmail.com>
2020-01-14 20:44:06 +0100

    Try new implem

--- hgw_opti.cu
+++ hgw_opti.cu
@@ -111,6 +111,8 @@ int main() {
 
 	data = (size_t*)malloc(sizeof(size_t) * height*width);
 
+	printf("%i", compare(5, 10, 0));
+
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
 			data[j + i * width] = (i * width) +j;
